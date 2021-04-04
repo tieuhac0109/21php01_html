9b5341fc0a13b68c25553e2419a557922fd2af8e
Đặng Văn Hiếu <64783557+tieuhac0109@users.noreply.github.com>
2021-04-04 15:38:52 +0700

    up ss8

---
+++ Session8/js/ss8.js
@@ -0,0 +1,14 @@
+function testExternal() {
+	document.getElementById("test_external").innerHTML = "Hello demo external";
+}
+
+function change_content() {
+	document.getElementById("change_content").innerHTML = "Đây là nội dung mới";
+}
+function change_content_2() {
+	document.getElementById("change_content").innerHTML = "Đây là nội dung đầu tiên";
+}
+
+function changeImage() {
+	document.getElementById("change_image").src = "img/image2.jpg"
+}
