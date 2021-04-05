f0cea9148e726d11ec125ad688512cddd4806c74
Dang Van Hieu <dangvanhieu@due.edu.vn>
2021-04-05 18:05:37 +0700

    up hw ss8

---
+++ Session8/homeworks/js/homeworkss8.4.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+$('#previous').on('click', function(){
+// Change to the previous image
+$('#im_' + currentImage).stop().fadeOut(1);
+decreaseImage();
+$('#im_' + currentImage).stop().fadeIn(1);
+});
+$('#next').on('click', function(){
+// Change to the next image
+$('#im_' + currentImage).stop().fadeOut(1);
+increaseImage();
+$('#im_' + currentImage).stop().fadeIn(1);
+});
+
+var currentImage = 1;
+var totalImages = 5;
+
+function increaseImage() {
+/* Increase currentImage by 1.
+* Resets to 1 if larger than totalImages
+*/
+++currentImage;
+if(currentImage > totalImages) {
+currentImage = 1;
+}
+}
+function decreaseImage() {
+	window.setInterval(function() {
+	$('#previous').click();
+	}, 2500);
+/* Decrease currentImage by 1.
+* Resets to totalImages if smaller than 1
+*/
+--currentImage;
+if(currentImage < 1) {
+currentImage = totalImages;
+}
+}
+});
