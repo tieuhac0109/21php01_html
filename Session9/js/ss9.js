27b761555cd6b7085f76b66ec7e12b099ef8ac36
Đặng Văn Hiếu <64783557+tieuhac0109@users.noreply.github.com>
2021-04-06 15:36:53 +0700

    up ss9

---
+++ Session9/js/ss9.js
@@ -0,0 +1,5 @@
+function changeInfo(name, image, age) {
+	document.getElementById('change-image').src = image;
+	document.getElementById('change-name').innerHTML = name;
+	document.getElementById('change-age').innerHTML = age;
+}
---
+++ Session9/js/studyss9.js
@@ -0,0 +1,63 @@
+function totalSum(a ,b) {
+	document.write(a + b);
+}
+totalSum(7, 8);
+// Bốn cách hiển thị dữ liệu trong javascript
+
+document.write("<h1> Demo show data </h1>");
+document.getElementById('demo').innerHTML = "Test show data again";
+alert('Test show')
+console.log('test thu');
+
+// Khai báo biến trong javascript
+// var userName;
+// var myAge = 20;
+
+// Câu điều kiện
+// if (điều kiện) {}
+// Nếu điều kiện đúng thì thực hiện lệnh, nếu sai thì không thực hiện
+
+// if (điều kiện) {
+// 	Nếu điều kiện đúng thì thực hiện câu lệnh ở đây
+// } else {
+// 	Nếu điều kiện sai thì thực hiện câu lệnh ở đây
+// }
+
+
+// if (điều kiện 1) {
+// 	Nếu điều kiện 1 đúng thì thực hiện câu lệnh ở đây
+// } else if (điều kiện 2) {
+// 	Nếu điều kiện 2 đúng thì thực hiện câu lệnh ở đây
+// } else {
+// 	Nếu điều kiện 1 và điều kiện 2 sai thì thực hiện câu lệnh ở đây
+// }
+
+
+// Câu điều kiện switch case
+// switch (biến) {
+// 	case giá trị 1:
+// 		Nếu thỏa giá trị 1 thì thực hiện câu lệnh ở đây
+// 		break; //bỏ qua các giá trị khác
+// 	case giá trị 2:
+// 		Nếu thỏa giá trị 2 thì thực hiện câu lệnh ở đây
+// 		break; //bỏ qua các giá trị khác
+// 	case giá trị 3:
+// 		Nếu thỏa giá trị 3 thì thực hiện câu lệnh ở đây
+// 		break; //bỏ qua các giá trị khác
+// 	default:
+// 		Nếu không thỏa tất cả các giá trị trên thì thực hiện câu lệnh ở đây
+// 		break; //bỏ qua các giá trị khác
+// }
+
+
+var month = 12;
+var year = 2021;
+
+if (month = 1, 3, 5, 7, 8, 10, 12) {
+	document.write(Tháng x có 31 ngày)
+} else if (month = 4, 6, 9, 11) {
+	document.write(Tháng x có 30 ngày)
+} else {
+	
+}
+
