b300fec3cef92a2a46b2c618407ce6d2ce95716a
Đặng Văn Hiếu <64783557+tieuhac0109@users.noreply.github.com>
2021-04-11 12:56:10 +0700

    up ss10

--- Session9/homeworks/js/homeworkss9.1.js
+++ Session9/homeworks/js/homeworkss9.1.js
@@ -1,9 +1,9 @@
 function checkYear(month, year) {
 	var month = 5;
 	var year = 2020;
-	if (month = 1, 3, 5, 7, 8, 10, 12) {
+	if (month = 1 || month = 3 || month = 5 || month = 7 || month = 8 || month = 10 || month = 12) {
 		document.write("Tháng x có 31 ngày");
-	} else if (month = 4, 6, 9, 11) {
+	} else if (month = 4 || month = 6 || month = 9 || month = 11) {
 		document.write("Tháng x có 30 ngày");
 	} else {
 		if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)) {
@@ -13,3 +13,4 @@ function checkYear(month, year) {
 		}
 	}
 }
+
