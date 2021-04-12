8d607b627cdc4f7a761e2da4824c0da755b51de7
Đặng Văn Hiếu <64783557+tieuhac0109@users.noreply.github.com>
2021-04-12 17:49:16 +0700

    up ss10

--- Session10/homeworks/js/homeworkss10.2.js
+++ Session10/homeworks/js/homeworkss10.2.js
@@ -13,6 +13,8 @@
 //Object kết quả
 var ketqua = document.getElementById('ketqua');
 
+//Vừa bấm phép tính
+var doi_phep_tinh = false;
 
 //Số phép tính ưu tiên tìm thấy
 var uu_tien = 0;
@@ -37,7 +39,7 @@ var phim_moi = '';
 function ClickButton(obj) {
 
 	//chuỗi hiện tại của kết quả
-	if (str_ketqua== '' && phim_moi == '') {
+	if (str_ketqua == '' && phim_moi == '') {
 		str_ketqua = ketqua.value;
 	}
 	if (str_ketqua == '0') {
@@ -48,7 +50,9 @@ function ClickButton(obj) {
 	var type = obj.innerHTML;
 
 	//Nhóm 1 số
-	if (	type == '0' ||
+	if (	type == '(' ||
+			type == ')' ||
+			type == '0' ||
 			type == '1' ||
 			type == '2' ||
 			type == '3' ||
@@ -58,19 +62,36 @@ function ClickButton(obj) {
 			type == '7' ||
 			type == '8' ||
 			type == '9' ||
-			type == '.' )
-		{
-		if (phim_moi += type) {
+			type == '.' ) {
+		doi_phep_tinh = false;
 		//Thay đổi hiển thị
-		 ketqua.value = str_ketqua + phim_moi;
-		} else () {
-
-		}
-
-		}
-
-		// else if (true) {}
+		phim_moi += type;
+		ketqua.value = str_ketqua + phim_moi;
+	} else if (	type == '(' ||
+				type == ')' ||
+				type == '+' ||
+				type == '-' ||
+				type == 'x' ||
+				type == '&divide;' ) {
+			//Trước đó đã có 1 phép tính
+			if (doi_phep_tinh) {
+				mang_pt[mang_pt_index-1] = type;
+				//Xử lý hiển thị
+				ketqua.value = ketqua.value.substring(0,ketqua.value.length-1) + type;
+			}
+			//Chưa có phép tính
+			else {
+				mang_so[mang_so_index] = parseFloat(phim_moi);
+				mang_so_index++;
+				mang_pt[mang_pt_index] = type;
+				mang_pt_index++;
+				//Xử lý hiển thị
+				ketqua.value = ketqua.value + type;
+			}
+			//Ghi nhớ đã bấm phím phép tính
+			doi_phep_tinh = true;
+			//Đổi giá trị 2 chuỗi cơ bản về rỗng
+			str_ketqua = '';
+			phim_moi = '';
 	}
-
-
-//https://www.youtube.com/watch?v=qe9dUQCqGM8
+}
