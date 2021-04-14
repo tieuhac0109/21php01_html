ee7d8a347db968bb94b963538c3cf441cc62078f
Đặng Văn Hiếu <64783557+tieuhac0109@users.noreply.github.com>
2021-04-14 17:48:08 +0700

    up

--- Session11/homeworks/js/homeworkss11.1.js
+++ Session11/homeworks/js/homeworkss11.1.js
@@ -1,18 +1,48 @@
-var myArray = [ 
- ["1","2","3","4","5","6","7","8","9"], 
- ["1","2","3","4","5","6","7","8","9"], 
- ["1","2","3","4","5","6","7","8","9"],
- ["1","2","3","4","5","6","7","8","9"],
- ["1","2","3","4","5","6","7","8","9"],
- ["1","2","3","4","5","6","7","8","9"] 
- ];
+// var row = "123456789";
+// var row1 = row;
+// var row2 = row;
+// var row3 = row;
+// var row4 = row;
+// var row5 = row;
 
- var myString="<table border=2>";
-   for (var y=0; y<myArray.length; y++ ) { // for each row
-      myString += "<br>";
-      for (var x=0; x<myArray.length; x++ ) { // for each clm
-         myString += "<td>" + myArray[y][x] + "<br>";
-      }
-       sOut += "</tr>";
-   }
-   sOut += "</table>";
+// document.write(row1 + "<br>" + row2 + "<br>" + row3 + "<br>" + row4 + "<br>" + row5);
+
+// document.write("<br>");
+// document.write("<br>");
+
+// row1Length = row1.length;
+// document.write(row1Length);
+// // myRow = row1.substr(row1.length / 2 , 1);
+// // document.write(myRow);
+// document.write("<br>");
+// document.write("<br>");
+// lengthRow = row.length / 2;
+// // var i = 1
+// // myRow1 = row1.replace(row1.substr(lengthRow--, i) , "*")
+// // document.write(myRow1);
+// // document.write("<br>");
+// // myRow2 = row2.replace(row2.substr(lengthRow--, i + 2) , "***")
+// // document.write(myRow2);
+// // document.write("<br>");
+// // myRow3 = row3.replace(row3.substr(lengthRow--, i + 2) , "*****")
+// // document.write(myRow3);
+// // document.write("<br>");
+// // myRow4 = row4.replace(row4.substr(lengthRow--, i + 2) , "*******")
+// // document.write(myRow4);
+// // document.write("<br>");
+// // myRow5 = row5.replace(row5.substr(lengthRow--, i + 2) , "*********")
+// // document.write(myRow5);
+// // document.write("<br>");
+
+var row = "123456789";
+lengthRow = row.length / 2;
+var star = "*";
+for (var i = 1; i <=9; i = i + 2) {
+	myRow = row.replace(row.substr(lengthRow, i) , star);
+	lengthRow--;
+	star++;
+	document.write(myRow + "<br>");
+
+
+}
+
